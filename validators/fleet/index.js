e74009e411bb1d796faa1098ef13ee4ba48d6261
malacled <00019257wiut@gmail.com>
2024-03-14 15:53:03 +0500

    fleetified productionDate

--- validators/fleet/index.js
+++ validators/fleet/index.js
@@ -6,12 +6,12 @@ const addFleetValidation = () => {
         body('eventName')
             .notEmpty().withMessage('Event name must not be empty')
             .isLength({ min: 8, max: 255 }).withMessage('Event name must be between 8 and 255 characters long'),
-        body('eventDateTime')
+        body('productionDate')
             .notEmpty().withMessage('Event date time must not be empty')
             .matches(/^(0[1-9]|[12][0-9]|3[01])\/(0[1-9]|1[012])\/(19|20)\d\d\s([01][0-9]|2[0-3]):([0-5][0-9])$/, 'g')
             .withMessage('Invalid date and time format. Please use "DD/MM/YYYY HH:mm" format.'),
-        body('venue')
-            .notEmpty().withMessage('Event venue must not be empty'),
+        body('carType')
+            .notEmpty().withMessage('Car Type cannot be empty.'),
         body('contactPhone')
             .notEmpty().withMessage('Contact phone must not be empty')
             .matches(/^\+998\d{9}$/).withMessage('Invalid phone number format, it must be +998xxxxxxxxx'),
@@ -42,12 +42,12 @@ const updateFleetValidation = () => {
         body('eventName')
             .notEmpty().withMessage('Event name must not be empty')
             .isLength({ min: 8, max: 255 }).withMessage('Event name must be between 8 and 255 characters long'),
-        body('eventDateTime')
+        body('productionDate')
             .notEmpty().withMessage('Event date time must not be empty')
             .matches(/^(0[1-9]|[12][0-9]|3[01])\/(0[1-9]|1[012])\/(19|20)\d\d\s([01][0-9]|2[0-3]):([0-5][0-9])$/, 'g')
             .withMessage('Invalid date and time format. Please use "DD/MM/YYYY HH:mm" format.'),
-        body('venue')
-            .notEmpty().withMessage('Event venue must not be empty'),
+        body('carType')
+            .notEmpty().withMessage('Car Type cannot be empty.'),
         body('contactPhone')
             .notEmpty().withMessage('Contact phone must not be empty')
             .matches(/^\+998\d{9}$/).withMessage('Invalid phone number format, it must be +998xxxxxxxxx'),
